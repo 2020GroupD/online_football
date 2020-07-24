44c8e1d14f688a816bc85ab30b2b9a7b889363ae
Imanity123 <1175729161@qq.com>
2020-07-24 16:58:14 +0800

    show_data_stream

---
+++ common/show_strength.c
@@ -0,0 +1,20 @@
+#include "head.h"
+
+struct Map court;
+WINDOW *Write;
+int sockfd;
+
+void show_strength() {
+	char tmp;
+	int len = 0;
+	struct Ctl ctll
+
+	while (scanf("%s", &tmp) != 'k') {
+		if (tmp == ' ') break;
+		len = (len + 1)%20;
+		wmove(Write, 6, len);
+	}
+	if ()
+
+
+}
